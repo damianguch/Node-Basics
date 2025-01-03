ab74eff769b3966ce29e80822a95514605824e9a
Damian Oguche <damianguch@yahoo.com>
2025-01-03 07:40:27 +0100

    Added imge sorting and pagination features to the app

--- controllers/image.controller.js
+++ controllers/image.controller.js
@@ -2,11 +2,11 @@ const { uploadImage } = require('../helper/cloudinary');
 Image = require('../models/image.model');
 const fs = require('fs');
 const cloudinary = require('../config/cloudinary');
+const { cursorTo } = require('readline');
 
 const uploadImageHandler = async (req, res) => {
   try {
     /**** HANDLE THE CASE FOR SINGLE FILE UPLOAD ****/
-
     // req.file is populated by upload.single() not upload.array(). So Update
     // the handler to consistently use req.files
     if (req.files && req.files.length === 1) {
@@ -86,18 +86,35 @@ const uploadImageHandler = async (req, res) => {
 
 const fetchAllImages = async (req, res) => {
   try {
-    const images = await Image.find({});
+    // Pagination
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 5;
+    const skip = (page - 1) * limit;
+
+    // Sorting
+    const sortBy = req.query.sortBy || 'createdAt';
+    const sortOrder = req.query.sortOrder === 'desc' ? -1 : 1;
+    const totalImages = await Image.countDocuments();
+    const totalPages = Math.ceil(totalImages / limit);
+
+    const sortObj = {};
+    sortObj[sortBy] = sortOrder;
+
+    const images = await Image.find().sort(sortObj).skip(skip).limit(limit);
 
     if (!images) {
       return res.status(400).json({
         success: false,
-        error: 'No imges found'
+        error: 'No images found'
       });
     }
 
     res.status(200).json({
       success: true,
       message: 'Images fetched successfully',
+      totalPages,
+      totalImages,
+      currentPage: page,
       data: images
     });
   } catch (error) {
