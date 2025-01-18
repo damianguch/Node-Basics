7f666a403d21af5029e4c908fee1dc6090b8cf72
Damian Oguche <damianguch@yahoo.com>
2025-01-18 07:53:12 +0100

    Refactored the code keeping passport routes in a module

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 require('./strategies/local-strategy');
 dotenv.config();
+const passportRoute = require('./passport/passport.route');
 const cartRoute = require('./routes/cart.route');
 const passport = require('passport');
 const homeRoute = require('./routes/home.route');
@@ -55,6 +56,7 @@ app.use('/api/products', productRoute);
 app.use('/api/books', bookRoute);
 app.use('/api/authors', authorRoute);
 app.use('/api/cart', cartRoute);
+app.use(passportRoute);
 
 app.get('/', (req, res) => {
   // Modify the session data object so that if cookie is not
@@ -64,31 +66,6 @@ app.get('/', (req, res) => {
   console.log(req.sessionID);
 
   res.render('index', { title: 'Home' });
-});
-
-// Using the callback version of passport.authenticate()
-// passport.authenticate() does not handle errors or customization
-// by default. Its default response s 401 Unauthorized.
-app.use('/api/authenticate', (req, res, next) => {
-  passport.authenticate('local', (err, user, info) => {
-    if (err) {
-      console.log('Authentication error: ', err);
-      return res.status(401).json({ error: err });
-    }
-
-    if (!user) {
-      return res.status(401).json({ error: info.error });
-    }
-
-    req.logIn(user, (err) => {
-      if (err) {
-        console.log(err);
-        return next(err);
-      }
-
-      return res.status(200).json({ message: 'Authenticated', user });
-    });
-  })(req, res, next);
 });
 
 // Check the status of the session authentication
@@ -106,32 +83,6 @@ app.get('/api/auth/status', (req, res) => {
   return req.session.auth
     ? res.status(200).json({ auth: true })
     : res.status(401).json({ auth: false, error: 'Not authenticated!' });
-});
-
-// Check the status of the passport session
-app.get('/api/auth/passport-status', (req, res) => {
-  console.log('Inside passport-status');
-  console.log(req.user);
-  console.log(req.session); // Passport modifies the session object
-
-  return req.user
-    ? res.status(200).json({ auth: true })
-    : res.status(401).json({ auth: false });
-});
-
-// Logout the user using the passport logout method
-app.post('/api/auth/logout', (req, res) => {
-  if (!req.user) {
-    return res.status(401).json({ error: 'User Unauthenticated' });
-  }
-  req.logout((err) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).json({ error: 'Logout error' });
-    }
-
-    return res.status(200).json({ message: 'User logged out' });
-  });
 });
 
 const start = async () => {
